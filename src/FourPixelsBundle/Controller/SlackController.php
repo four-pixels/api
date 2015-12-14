c729741852691d6636b378786d0f658102ff0c97
Rene Ramirez <rene.ramirez@fersz.com>
2015-12-13 22:40:30 -0500

    first commit

---
+++ src/FourPixelsBundle/Controller/SlackController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace FourPixelsBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class SlackController extends FOSRestController {
+
+  /**
+   * Do something.
+   *
+   * @ApiDoc(
+   *   resource = true,
+   *   statusCodes = {
+   *     200 = "Returned when successful"
+   *   }
+   * )
+   *
+   * @Annotations\View()
+   *
+   * @param Request               $request      the request object
+   * @param ParamFetcherInterface $paramFetcher param fetcher service
+   *
+   * @return array
+   */
+  public function getSlackAction(Request $request, ParamFetcherInterface $paramFetcher) {
+    return ['message' => 'hello'];
+  }
+  /**
+   * Do something.
+   *
+   * @ApiDoc(
+   *   statusCodes = {
+   *     200 = "Returned when successful"
+   *   }
+   * )
+   *
+   * @Annotations\View()
+   *
+   * @param Request               $request      the request object
+   * @param ParamFetcherInterface $paramFetcher param fetcher service
+   *
+   * @return array
+   */
+  public function getSlackInstructionsAction(Request $request, ParamFetcherInterface $paramFetcher) {
+    return ['message' => 'testing the api - not much yet, may the force be with you.'];
+  }
+  /**
+   * Do something.
+   *
+   * @ApiDoc(
+   *   statusCodes = {
+   *     200 = "Returned when successful"
+   *   }
+   * )
+   *
+   * @Annotations\View()
+   *
+   * @param Request               $request      the request object
+   * @param ParamFetcherInterface $paramFetcher param fetcher service
+   *
+   * @return array
+   */
+  public function getSlackSupportAction(Request $request, ParamFetcherInterface $paramFetcher) {
+    return ['message' => 'some support stuff required, may the force be with you.'];
+  }
+  /**
+   * Do something.
+   *
+   * @ApiDoc(
+   *   statusCodes = {
+   *     200 = "Returned when successful"
+   *   }
+   * )
+   *
+   * @Annotations\View()
+   *
+   * @param Request               $request      the request object
+   * @param ParamFetcherInterface $paramFetcher param fetcher service
+   *
+   * @return array
+   */
+  public function getSlackCallbackAction(Request $request, ParamFetcherInterface $paramFetcher) {
+    return ['message' => 'some callback function here, may the force be with you.'];
+  }
+
+}
---
+++ src/FourPixelsBundle/FourPixelsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FourPixelsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FourPixelsBundle extends Bundle
+{
+}
