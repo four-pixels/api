8759ccf96240fa02ac7e96e1ab6fde5b853d71f4
Rene Ramirez <rene.ramirez@fersz.com>
2015-12-16 01:39:15 -0500

    add post request

--- src/FourPixelsBundle/Controller/SlackController.php
+++ src/FourPixelsBundle/Controller/SlackController.php
@@ -92,6 +92,43 @@ class SlackController extends FOSRestController {
    * @return array
    */
   public function getSlackCallbackAction(Request $request, ParamFetcherInterface $paramFetcher) {
+    /* @var $client \Guzzle\Http\Client */
+    /* @var $requestForSlack \Guzzle\Http\Message\Request */
+    $logger = $this->get('logger');
+    if ($request->isMethod('GET')) {
+      $logger->info("----------------------------------------------");
+      $logger->info("----------PARAMETERS FROM SLACK GET-----------");
+      $logger->info("----------------------------------------------");
+      $slackCode = $request->get('code');
+      $slackState = $request->get('state');
+      $logger->info("CODE -> " . $slackCode);
+      $logger->info("STATE -> " . $slackState);
+    } else if ($request->isMethod('POST')) {
+      
+    }
+
+
+    // Create a client and provide a base URL
+    $client = new \Guzzle\Http\Client('https://slack.com');
+    $client->setDefaultOption('query', [
+        'client_id' => $this->getParameter('slack.client_id'),
+        'client_secret' => $this->getParameter('slack.client_secret'),
+        'code' => $slackCode,
+    ]);
+    $requestForSlack = $client->get('/api/oauth.access');
+    $response = $requestForSlack->send();
+    $b = $response->getBody();
+    $logger->info($b);
+    $l = $response->getHeader('Content-Length');
+    $logger->info($l);
+    $data = $response->json();
+    $logger->info($data);
+
+
+    $logger->info("----------------------------------------------");
+    $logger->info($request);
+    $logger->info("*************************************************");
+    $logger->info("----------------------------------------------");
     return ['message' => 'some callback function here, may the force be with you.'];
   }
 
@@ -113,8 +150,14 @@ class SlackController extends FOSRestController {
    *
    * @return array
    */
-  public function getSlackTeamtreehouseAction(Request $request, ParamFetcherInterface $paramFetcher) {
-    return ['text' => 'Let the Team Tree House tournament begin, may the force be with you.'];
+  public function postSlackTeamtreehouseAction(Request $request, ParamFetcherInterface $paramFetcher) {
+    return [
+        'username' => 'pixels bot',
+        'text' => 'Let the Team Tree House tournament begin, may the force be with you.',
+        "channel" => "#testing-api",
+        "icon_url" => "https://slack.com/img/icons/app-57.png",
+        "icon_emoji" => ":ghost:"
+    ];
   }
 
 }
